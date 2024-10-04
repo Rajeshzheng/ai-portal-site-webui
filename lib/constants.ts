2b7ea67713dae0d99471463871eab4d192c04099
Rajeshzheng <yongyuan11174242@gmail.com>
2024-10-04 16:03:47 +0800

    update sumbit page copies

--- lib/constants.ts
+++ lib/constants.ts
@@ -21,10 +21,10 @@ export const RevalidateOneMonth = 3600 * 24 * 30;
 export const PAGE_SIZE = 20;
 export const InfoPageSize = 12;
 
-export const FORM_PLACEHOLDER = 'https://tap4.ai/';
+export const FORM_PLACEHOLDER = 'https://aibasket.me/';
 
 export const WEBSITE_EXAMPLE =
-  '<a href="https://tap4.ai/" title="Tap4 AI Tools Directory">Tap4 AI Tools Diresctory</a>';
+  '<a href="https://aibasket.me/" title="AI Basket Tools Directory">AI Basket Tools Directory</a>';
 
 export const STARTUP_LIST = [
   {
